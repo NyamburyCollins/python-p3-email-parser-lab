e2e2af7fa14567b3afcd40224d51f496fb991d15
*Collins <nyamburycollins@mail.com>
2025-05-23 08:33:02 +0300

    completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    def __init__(self, emails):
+        self.emails = emails
+
+    def parse(self):
+        # Split emails using both commas and spaces as separators
+        split_emails = re.split(r'[, ]+', self.emails)
+        
+        # Filter valid emails using regex pattern
+        email_pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        valid_emails = [email for email in split_emails if re.match(email_pattern, email)]
+        
+        # Remove duplicates and sort alphabetically
+        unique_emails = list(set(valid_emails))
+        unique_emails.sort()
+        
+        return unique_emails
